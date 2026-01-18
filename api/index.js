f1c686de1a20b0e88ba3de0dd4cd81b65341ee4e
learnervivek <v8381813340@gmail.com>
2026-01-19 01:20:03 +0530

    Add Vercel API handler

--- api/index.js
+++ api/index.js
@@ -1,11 +1,1 @@
-import app from "../backend/app.js";
-import cloudinary from "cloudinary";
-
-// Configure Cloudinary for serverless
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-export default app;
+export { default } from '../backend/server.js'
